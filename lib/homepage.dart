02a3186f88008b90cef57eba83eb3696d0d0e2d4
darshanaswath <39420536+darshanaswath@users.noreply.github.com>
2020-07-14 11:48:09 +0530

    Division + UV

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -352,11 +352,144 @@ class _MyHomePageState extends State<MyHomePage>
             SizedBox(
               height: 15,
             ),
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 28.0, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Pollutants",
+                      style: GoogleFonts.ubuntu(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.black87),
+                    ),
+                    Icon(
+                      Icons.info_outline,
+                      color: Colors.black38,
+                    )
+                  ],
+                ),
+              ),
+            ),
             SingleChildScrollView(
               physics: BouncingScrollPhysics(),
               scrollDirection: Axis.horizontal,
               child: Row(
                 children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Container(
+                      height: 180,
+                      width: 230,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                            top: 8.0, bottom: 8, left: 20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "UV Index",
+                              style: GoogleFonts.ubuntu(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.black38),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: [
+                                CircularPercentIndicator(
+                                  animation: true,
+                                  arcType: ArcType.HALF,
+                                  radius: 100.0,
+                                  lineWidth: 12.0,
+                                  percent: 0.47,
+                                  animationDuration: 1500,
+                                  center: new Text(
+                                    "2nm",
+                                    style: GoogleFonts.ubuntu(
+                                        fontSize: 20, fontWeight: FontWeight.bold),
+                                  ),
+                                  linearGradient: LinearGradient(
+                                    colors: [
+                                      Colors.black,
+                                      Colors.red,
+                                      Colors.purpleAccent
+                                    ],
+                                  ),
+                                  backgroundColor: Colors.white,
+                                  circularStrokeCap: CircularStrokeCap.round,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 15.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Container(
+                                            width: 15,
+                                            height: 15,
+                                            decoration: new BoxDecoration(
+                                              color: Colors.black,
+                                              shape: BoxShape.circle,
+                                            ),
+                                          ),
+                                          SizedBox(width: 10,),
+                                          Text('Low', style: GoogleFonts.ubuntu(fontSize: 17,fontWeight: FontWeight.bold),),
+                                        ],
+                                      ),
+                                      SizedBox(height: 10,),
+                                      Row(
+                                        children: [
+                                          Container(
+                                            width: 15,
+                                            height: 15,
+                                            decoration: new BoxDecoration(
+                                              color: Colors.red,
+                                              shape: BoxShape.circle,
+                                            ),
+                                          ),
+                                          SizedBox(width: 10,),
+                                          Text('Mid', style: GoogleFonts.ubuntu(fontSize: 17,fontWeight: FontWeight.bold),),
+                                        ],
+                                      ),
+                                      SizedBox(height: 10,),
+                                      Row(
+                                        children: [
+                                          Container(
+                                            width: 15,
+                                            height: 15,
+                                            decoration: new BoxDecoration(
+                                              color: Colors.purpleAccent,
+                                              shape: BoxShape.circle,
+                                            ),
+                                          ),
+                                          SizedBox(width: 10,),
+                                          Text('High', style: GoogleFonts.ubuntu(fontSize: 17,fontWeight: FontWeight.bold),),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                   Padding(
                     padding: const EdgeInsets.all(12.0),
                     child: Container(
@@ -421,7 +554,7 @@ class _MyHomePageState extends State<MyHomePage>
                                     height: 30,
                                     child: CachedNetworkImage(
                                         imageUrl:
-                                        "https://media1.giphy.com/media/h4OGa0npayrJX2NRPT/source.gif"))
+                                            "https://media1.giphy.com/media/h4OGa0npayrJX2NRPT/source.gif"))
                               ],
                             ),
                             LinearPercentIndicator(
@@ -507,7 +640,7 @@ class _MyHomePageState extends State<MyHomePage>
                                     height: 30,
                                     child: CachedNetworkImage(
                                         imageUrl:
-                                        "https://i.pinimg.com/originals/0e/3e/e5/0e3ee551876e1ad2a39f89e4adf9168a.gif"))
+                                            "https://i.pinimg.com/originals/0e/3e/e5/0e3ee551876e1ad2a39f89e4adf9168a.gif"))
                               ],
                             ),
                             LinearPercentIndicator(
@@ -593,7 +726,7 @@ class _MyHomePageState extends State<MyHomePage>
                                     height: 30,
                                     child: CachedNetworkImage(
                                         imageUrl:
-                                        "https://media1.giphy.com/media/gfI2SFqrtxwf2Aw9x5/giphy.gif"))
+                                            "https://media1.giphy.com/media/gfI2SFqrtxwf2Aw9x5/giphy.gif"))
                               ],
                             ),
                             LinearPercentIndicator(
@@ -679,7 +812,7 @@ class _MyHomePageState extends State<MyHomePage>
                                     height: 30,
                                     child: CachedNetworkImage(
                                         imageUrl:
-                                        "https://media0.giphy.com/media/QWvra259h4LCvdJnxP/giphy.gif"))
+                                            "https://media0.giphy.com/media/QWvra259h4LCvdJnxP/giphy.gif"))
                               ],
                             ),
                             LinearPercentIndicator(
@@ -765,7 +898,7 @@ class _MyHomePageState extends State<MyHomePage>
                                     height: 30,
                                     child: CachedNetworkImage(
                                         imageUrl:
-                                        "https://media0.giphy.com/media/QWvra259h4LCvdJnxP/giphy.gif"))
+                                            "https://media0.giphy.com/media/QWvra259h4LCvdJnxP/giphy.gif"))
                               ],
                             ),
                             LinearPercentIndicator(
